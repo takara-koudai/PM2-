8dda92f1dcc5a654b32f343e89a9483c1a93a53d
kodai <kodai@MSI>
2022-10-19 14:51:09 +0900

    2行目にコンクリプトを追加

--- PM2基本操作.cpp
+++ PM2基本操作.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello GitHub World!\n";
 
-    std::cout << "Hello Branch!\n";
+    std::cout << "Hello コンクリプトBranch!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
